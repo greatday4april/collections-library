f42818d3576741942651fa2073301a20c5583d19
April Li <greatday4april@gmail.com>
2020-09-06 10:32:33 -0700

    use the same pattern as ruby bsearch

--- bsearch/bsearch.py
+++ bsearch/bsearch.py
@@ -1,11 +1,16 @@
 from typing import List, Callable, Optional
 
+"""
+condition should be a function that evaluates as
+[False, False ... True, True, True], aka >= target
+"""
+
 
 def bsearch_low(arr: List[int], condition: Callable[[int], bool]) -> Optional[int]:
     low = 0
     high = len(arr)
     while low < high:
-        mid = low + (high - low) // 2
+        mid = low + (high - low) // 2  # closer to low
         smaller = condition(arr[mid])
         # keep this upper-bound while trying lower upper-bound
         if smaller:
@@ -19,11 +24,19 @@ def bsearch_low(arr: List[int], condition: Callable[[int], bool]) -> Optional[in
     return low
 
 
+"""
+condition should be a function that evaluates as
+[True, True ... False, False, False], aka <= target
+"""
+
+
 def bsearch_high(arr: List[int], condition: Callable[[int], bool]) -> Optional[int]:
     low = 0
     high = len(arr)
     while low < high:
-        mid = low + (high - low) // 2
+        mid = high - (high - low) // 2  # closer to high
+        if mid >= len(arr):
+            break
         bigger = condition(arr[mid])
         # keep this lower-bound while trying higher lower-bound
         if bigger:
@@ -38,22 +51,28 @@ def bsearch_high(arr: List[int], condition: Callable[[int], bool]) -> Optional[i
 
 
 if __name__ == '__main__':
-    print(bsearch_low([], lambda ele: ele == 2))  # None
-    print(bsearch_high([], lambda ele: ele == 2))  # None
-    print(bsearch_low([1], lambda ele: ele == 2))  # None
-    print(bsearch_low([1], lambda ele: ele == 2))  # None
+    print(bsearch_low([], lambda ele: ele >= 2))  # None
+    print(bsearch_high([], lambda ele: ele >= 2))  # None
+    print(bsearch_low([1], lambda ele: ele >= 2))  # None
+    print(bsearch_high([1], lambda ele: ele <= 0))  # None
     print('--------')
 
-    print(bsearch_low([1, 2, 3, 4, 5, 6], lambda ele: ele == 4))  # 3
-    print(bsearch_high([1, 2, 3, 4, 5, 6], lambda ele: ele == 4))  # 3
+    print(bsearch_low([1], lambda ele: ele >= 1))  # 0
+    print(bsearch_low([1], lambda ele: ele <= 1))  # 0
+    print(bsearch_low([1, 3], lambda ele: ele >= 1))  # 0
+    print(bsearch_high([1, 3], lambda ele: ele <= 1))  # 0
     print('--------')
 
-    print(bsearch_low([1, 2, 4, 4, 4, 5], lambda ele: ele == 4))  # 2
+    print(bsearch_low([1, 2, 3, 4, 5, 6], lambda ele: ele >= 4))  # 3
+    print(bsearch_high([1, 2, 3, 4, 5, 6], lambda ele: ele <= 4))  # 3
+    print('--------')
+
+    print(bsearch_low([1, 2, 4, 4, 4, 5], lambda ele: ele >= 4))  # 2
     print(bsearch_low([1, 2, 4, 4, 4, 5], lambda ele: ele > 5))  # None
     print(bsearch_low([1, 2, 4, 4, 4, 5], lambda ele: ele >= 3))  # 2
     print('--------')
 
-    print(bsearch_high([1, 2, 4, 4, 4, 5], lambda ele: ele == 4))  # 4
+    print(bsearch_low([1, 2, 4, 4, 4, 5], lambda ele: ele >= 4))  # 2
     print(bsearch_high([1, 2, 4, 4, 4, 5], lambda ele: ele <= 4))  # 4
     print('--------')
 
--- bsearch/bsearch.rb
+++ bsearch/bsearch.rb
@@ -1,9 +1,13 @@
 class Array
+  ''"
+  condition should be a function that evaluates as
+  [False, False ... True, True, True], aka >= target
+  "''
   def bsearch_low(&block)
     low = 0
     high = length
     while low < high
-      mid = low + (high - low) / 2
+      mid = low + (high - low) / 2 # closer to low
       smaller = block.call(self[mid])
       # keep this upper-bound while trying lower upper-bound
       if smaller
@@ -17,11 +21,17 @@ class Array
     low
   end
 
+  ''"
+  condition should be a function that evaluates as
+  [True, True ... False, False, False], aka <= target
+  "''
   def bsearch_high(&block)
     low = 0
     high = length
     while low < high
-      mid = low + (high - low) / 2
+      mid = high - (high - low) / 2 # closer to high
+      break if self[mid].nil?
+
       bigger = block.call(self[mid])
       # keep this lower-bound while trying higher lower-bound
       if bigger
@@ -37,20 +47,26 @@ class Array
 end
 
 if __FILE__ == $PROGRAM_NAME
-  p [0].bsearch_low { |ele| ele == 2 } # nil
-  p [0].bsearch_high { |ele| ele == 2 } # nil
-  p [1].bsearch_low { |ele| ele == 2 } # nil
-  p [1].bsearch_high { |ele| ele == 2 } # nil
+  p [].bsearch_low { |ele| ele >= 2 } # nil
+  p [].bsearch_high { |ele| ele >= 2 } # nil
+  p [1].bsearch_low { |ele| ele >= 2 } # nil
+  p [1].bsearch_high { |ele| ele <= 0 } # nil
 
-  p [1, 2, 3, 4, 5, 6].bsearch_low { |ele| ele == 4 } # 3
-  p [1, 2, 3, 4, 5, 6].bsearch_high { |ele| ele == 4 } # 3
+  p [1].bsearch_low { |ele| ele >= 1 } # 0
+  p [1].bsearch_high { |ele| ele <= 1 } # 0
 
-  p [1, 2, 4, 4, 4, 5].bsearch_low { |ele| ele == 4 } # 2
+  p [1, 3].bsearch_low { |ele| ele >= 1 } # 0
+  p [1, 3].bsearch_high { |ele| ele <= 1 } # 0
+
+  p [1, 2, 3, 4, 5, 6].bsearch_low { |ele| ele >= 4 } # 3
+  p [1, 2, 3, 4, 5, 6].bsearch_high { |ele| ele <= 4 } # 3
+
+  p [1, 2, 4, 4, 4, 5].bsearch_low { |ele| ele >= 4 } # 2
   p [1, 2, 4, 4, 4, 5].bsearch_low { |ele| ele > 5 } # nil
   p [1, 2, 4, 4, 4, 5].bsearch_low { |ele| ele >= 3 } # 2
 
-  p [1, 2, 4, 4, 4, 5].bsearch_high { |ele| ele == 4 } # 4
   p [1, 2, 4, 4, 4, 5].bsearch_high { |ele| ele <= 4 } # 4
   p [1, 2, 4, 4, 4, 5].bsearch_high { |ele| ele <= 0 } # nil
-  p [1, 2, 4, 4, 4, 5].bsearch_high { |ele| ele > 5 } # nil
+  p [1, 2, 4, 4, 4, 5].bsearch_high { |ele| ele < 5 } # 4
+
 end
