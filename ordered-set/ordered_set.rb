4588d8543bc16bac7f430bde261839ff7b60c2b9
April Li <greatday4april@gmail.com>
2020-10-19 15:36:08 -0700

    ordered set

---
+++ ordered-set/ordered_set.rb
@@ -0,0 +1,30 @@
+# works for Ruby 1.9+ as hash respects insertion order
+class OrderedSet
+  def initialize
+    @hash = {}
+  end
+
+  def add(element)
+    @hash[element] = element
+  end
+
+  def delete(element)
+    @hash.delete(element)
+  end
+
+  def include?(element)
+    @hash.key?(element)
+  end
+
+  def shift()
+    @hash.shift.first
+  end
+
+  def first()
+    @hash.first.first
+  end
+
+  def keys
+    @hash.keys
+  end
+end
